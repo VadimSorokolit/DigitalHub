37e8d37cb7b5b9d860e48bb0d5f64c1e53b0d47a
VadimSorokolit <macintosh@ukr.net>
2025-06-17 09:17:23 +0300

    Added NetworkMonitor for connectivity tracking

---
+++ DigitalHub/DigitalHub/Sevices/NetworkMonitor.swift
@@ -0,0 +1,33 @@
+//
+//  Untitled.swift
+//  DigitalHub
+//
+//  Created by Vadim Sorokolit on 17.06.2025.
+//
+    
+import Foundation
+import Network
+import Combine
+
+final class NetworkMonitor: ObservableObject {
+    static let shared = NetworkMonitor()
+
+    private let monitor: NWPathMonitor
+    private let queue = DispatchQueue(label: "NetworkMonitorQueue")
+    
+    @Published var isConnected: Bool = false
+
+    private init() {
+        monitor = NWPathMonitor()
+        monitor.pathUpdateHandler = { [weak self] path in
+            DispatchQueue.main.async {
+                self?.isConnected = (path.status == .satisfied)
+            }
+        }
+        monitor.start(queue: queue)
+    }
+    
+    deinit {
+        monitor.cancel()
+    }
+}
